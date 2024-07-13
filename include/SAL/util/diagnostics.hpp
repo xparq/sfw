64efb9f6bf96a8d0a41c7526d994749ebd862729
sz <698307+xparq@users.noreply.github.com>
2024-07-13 05:22:01 +0200

    Trapping asserts
    -> https://nullprogram.com/blog/2022/06/26/ by Chris Wellons (Skeeto)
    
    Called SAL_ASSERT for now, because of some unresolved macro conflicts
    (possibly due to the SFML interface headers also including <cassert>):
    despite redefining (#undef + #define) here, an assert(0) still did the
    same old standard print + abort thing! :-o

--- include/SAL/util/diagnostics.hpp
+++ include/SAL/util/diagnostics.hpp
@@ -1,16 +1,46 @@
 ﻿#ifndef _DFGNUDOSTDF8DM2HE87TYG87HY34870DFHYE98_
 #define _DFGNUDOSTDF8DM2HE87TYG87HY34870DFHYE98_
 
+//============================================================================
 #ifdef DEBUG
+//============================================================================
 
-	#include <cassert>
-	#include <iostream>
+//
+// Just have stderr handy...
+//
+//!! Grow a proper logging facility instead (or in addition), of course!
+//!! (Don't forget to use OutputDebugString on Windows, like in my old DBG header!)
+//
+#include <iostream>
 	using std::cerr, std::endl, std::hex;
 
-#else
+//
+// Pimped asserts from Chris Wellons (Skeeto) -- https://nullprogram.com/blog/2022/06/26/
+// Instead of #include <cassert>, this breaks (for debugging) instead of printing and terminating.
+//
 
-	// ...
+//!!?? WTF, I sill got the stock assert despite this?!
+//!!??#  undef assert // SFML could've already included <cassert> before we get here...
+//!!??
 
-#endif
+#  if __GNUC__
+#    define SAL_ASSERT(c) if (!(c)) __builtin_trap()
+#  elif _MSC_VER
+#    define SAL_ASSERT(c) if (!(c)) __debugbreak()
+#  else
+#    define SAL_ASSERT(c) if (!(c)) *(volatile int *)0 = 0
+#  endif
+
+//============================================================================
+#else // DEBUG
+//============================================================================
+
+
+#  define SAL_ASSERT(c)
+
+
+//============================================================================
+#endif // DEBUG
+//============================================================================
 
 #endif // _DFGNUDOSTDF8DM2HE87TYG87HY34870DFHYE98_
